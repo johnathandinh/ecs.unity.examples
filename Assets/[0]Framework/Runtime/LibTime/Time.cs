8b6199ed61bd18f1b2c507a46354d5d40957f0df
Pixeye <trqe@mail.ru>
2019-02-24 10:29:03 +0200

    Update

--- Assets/[0]Framework/Runtime/LibTime/Time.cs
+++ Assets/[0]Framework/Runtime/LibTime/Time.cs
@@ -6,14 +6,14 @@ namespace Homebrew
 	public class Time : ITick, IKernel, IComponent
 	{
 		protected const float fps = 60;
-
-
+		
 		/// <summary>
 		/// <para> Default Framework Time. Use it for independent timing</para>
 		/// </summary>
-		public static Time Default;
+		public static Time Default = new Time();
 		public static int frame => UnityEngine.Time.frameCount;
-       
+	 
+	 
 		/// <summary>
 		///   <para> The scale at which the time is passing. This can be used for slow motion effects.</para>
 		/// </summary>
@@ -22,14 +22,12 @@ namespace Homebrew
 
 		protected float _deltaTimeFixed;
 		protected float _deltaTime;
-
+		
 		internal float timeScaleCached = 1.0f;
-
 		/// <summary>
 		/// <para> The time in seconds it took to complete the last frame</para>
 		/// </summary>
 		public static float delta => Default._deltaTime;
-
 		/// <summary>
 		/// <para> 1 / 60 constant time</para>
 		/// </summary>
